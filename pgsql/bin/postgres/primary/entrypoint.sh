e154ef949349f09dd6eb70f7d7d8ad6fe2cfc3bc
Jan Guth <jan.guth@gmail.com>
2017-11-05 14:59:03 +0100

    clean up

--- pgsql/bin/postgres/primary/entrypoint.sh
+++ pgsql/bin/postgres/primary/entrypoint.sh
@@ -4,7 +4,7 @@ REPLICATION_PASSWORD=$(get_secret REPLICATION_PASSWORD)
 
 FORCE_RECONFIGURE=1 postgres_configure
 
-echo ">>> Creating replication user '$REPLICATION_USER' -- $REPLICATION_PASSWORD --"
+echo ">>> Creating replication user '$REPLICATION_USER'"
 psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE ROLE $REPLICATION_USER WITH REPLICATION PASSWORD '$REPLICATION_PASSWORD' SUPERUSER CREATEDB  CREATEROLE INHERIT LOGIN;"
 
 echo ">>> Creating replication db '$REPLICATION_DB'"
